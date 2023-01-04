12c8eb87d0d52ddb1cb6cc4a265959f0f060ec2f
felipeam10 <felipeam10@hotmail.com>
2023-01-04 13:49:24 -0300

    deve incluir conta com sucesso

--- src/main/java/br/fe/felipe/rest/tests/BarrigaTest.java
+++ src/main/java/br/fe/felipe/rest/tests/BarrigaTest.java
@@ -3,6 +3,9 @@ package br.fe.felipe.rest.tests;
 import br.fe.felipe.rest.core.BaseTest;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class BarrigaTest extends BaseTest {
@@ -14,6 +17,32 @@ public class BarrigaTest extends BaseTest {
                 .get("/contas")
         .then()
                 .statusCode(401)
+        ;
+
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso(){
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "felipeam10@hotmail.com");
+        login.put("senha", "123456");
+
+        String token = given()
+                .body(login)
+        .when()
+                .post("/signin")
+        .then()
+                .statusCode(200)
+                .extract().path("token")
+        ;
+
+        given()
+                .header("Authorization", "JWT " + token)
+                .body("{\"nome\": \"conta qualquer2\"}")
+        .when()
+                .post("/contas")
+        .then()
+                .statusCode(201)
         ;
 
     }
