dad802970d407d979750ea924421647d43f2c78f
felipeam10 <felipeam10@hotmail.com>
2023-01-06 15:21:42 -0300

    nao deve remover conta com movimentacoes

--- src/main/java/br/fe/felipe/rest/tests/BarrigaTest.java
+++ src/main/java/br/fe/felipe/rest/tests/BarrigaTest.java
@@ -140,6 +140,19 @@ public class BarrigaTest extends BaseTest {
 
     }
 
+    @Test
+    public void naoDeveRemoverContaComMovimentacao(){
+        given()
+                .header("Authorization", "JWT " + TOKEN)
+        .when()
+                .delete("/contas/1541375")
+        .then()
+                .statusCode(500)
+                .body("constraint", is("transacoes_conta_id_foreign"))
+        ;
+
+    }
+
     private Movimentacao getMovimentacaoValida(){
         Movimentacao mov = new Movimentacao();
         mov.setConta_id(1541376);
